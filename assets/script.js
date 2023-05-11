2bbde653f9ca5c8bbca4c34b8967e54ee7273e8b
enanesh <edna.ganesha.gr@gmail.com>
2023-05-10 18:09:39 -0700

    Basic CSS and HTML changes to take user input, creates the function to save user input in localStorage

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,27 @@
 var city = "";
 var lon = "";
 var lat = "";
+
+
+//Saves the user input into localstorage 
+
+function SaveInput() {
+    document.getElementById('button').addEventListener("click", function (e) {
+        e.preventDefault();
+        var city = document.getElementById("search-input").value;
+        var item = JSON.parse(localStorage.getItem("cities"));
+        if (item == null) {
+            item = [city];
+        } else {
+            if (!item.includes(city)) {
+                item.push(city)
+            }
+        }
+        localStorage.setItem("cities",JSON.stringify(item))
+    })
+}
+
+
+
+console.log(city)
+SaveInput()
